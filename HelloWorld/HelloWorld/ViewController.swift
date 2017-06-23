8f53c9f3a7f187b5138598c42f655cf6a15291a2
Shiyao <liushiyao2014@gmail.com>
2017-06-22 22:14:53 -0500

    Add boilerplate project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Shiyao Liu on 6/22/17.
+//  Copyright © 2017 Shiyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
